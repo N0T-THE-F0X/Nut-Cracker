cfb5d5bb1748c98ecd8756073cc5a08cdd4ada41
ALX <115327608+N0T-THE-F0X@users.noreply.github.com>
2023-04-06 13:54:30 -0600

    First upload

---
+++ 1.1crack.cpp
@@ -0,0 +1,35 @@
+//Libraries:
+#include <iostream>
+#include <chrono>
+#include <string>
+using namespace std;
+
+//Variabels:
+string alphabet = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890`~!@#$%^&*()_-+=|}{][:;><.,?/";
+string password, guess, curentletter, crack;
+int i = 0, x =0;
+bool check = true;
+
+//Program:
+int main(){
+    cout << "Password: "; cin >> password;  //Asks for and saves your password
+    auto start_time = chrono::high_resolution_clock::now(); //Starts the stop watch  
+    while(check){
+        guess = alphabet[i]; curentletter = password[x]; // restates the curent letters
+
+        if(curentletter == guess){ //checks if its equal 
+            x++; crack = crack + guess;
+            
+            if(password == crack){ //checks if the whole password has been cracked
+                auto end_time = chrono::high_resolution_clock::now(); //ends the stopwatch
+                auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time); // 
+                cout << "\nPassword: " << password;
+                cout << "\nCracked: " << crack;
+                cout << "\nElapsed time: " << duration.count() << " microseconds.";
+                check = false;
+            }
+        }
+        i++;
+    }
+return(1);
+}
